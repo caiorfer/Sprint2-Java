f9a43b391690e9f3e4632422daf82927ad288578
Caio Rocha Fernandes <caiorf2005@gmail.com>
2024-05-18 12:28:28 -0300

    Delete Sprint2 directory

--- Sprint2/src/main/java/com/example/demo/repositories/ItemRecomendadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.ItemRecomendado;
-
-public interface ItemRecomendadoRepository extends JpaRepository<ItemRecomendado, Long> {
-
-}
