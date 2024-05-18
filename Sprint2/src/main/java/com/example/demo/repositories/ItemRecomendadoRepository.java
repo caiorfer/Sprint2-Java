e2ffe995f55baa1ca7d4232d5d4e7f72473e1aa1
Caio Rocha Fernandes <caiorf2005@gmail.com>
2024-05-18 12:52:01 -0300

    Add files via upload

---
+++ Sprint2/src/main/java/com/example/demo/repositories/ItemRecomendadoRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.ItemRecomendado;
+
+public interface ItemRecomendadoRepository extends JpaRepository<ItemRecomendado, Long> {
+
+}
